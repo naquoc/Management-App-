0891ecf6f5d784c4660a93a6323004b1dca015d8
quocna <quocnguyenanh96@gmail.com>
2023-03-23 12:37:26 +0700

    route grouping and init llayout

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,20 @@
+
+import GlassPane from "@/components/GlassPane"
+import { FC } from "react"
+import '@/styles/global.css'
+
+const DashboardRootLayout: FC<any> = ({ children }) => {
+  return (
+    <html lang='en'>
+      <head>
+        <body className="h-screen w-screen rainbow-mesh p-6">
+          <GlassPane className='w-full h-full flex items-center justify-center'>
+            {children}
+          </GlassPane>
+        </body>
+      </head>
+    </html>
+  )
+}
+
+export default DashboardRootLayout
